be9f61f901cb91c8f86fc3cd951b0f6ec73576a8
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2025-02-27 22:56:35 +0200

    exclude openapi paths from authentication

--- src/main/kotlin/com/nutrike/core/config/AuthenticationConfig.kt
+++ src/main/kotlin/com/nutrike/core/config/AuthenticationConfig.kt
@@ -1,9 +1,9 @@
 package com.nutrike.core.config
 
-class AuthentificationConfig {
+class AuthenticationConfig {
     companion object {
         // Paths that will be excluded from authentication
-        val AUTHENTICATION_EXCLUDE = listOf("/auth")
+        val AUTHENTICATION_EXCLUDE = listOf("/actuator/**", "/auth", "/swagger-ui/**", "/v3/api-docs/**")
         const val TOKEN_PREFIX = "Bearer "
         const val AUTHENTICATION_HEADER = "Authorization"
         const val TOKEN_EXPIRATION_TIME = 1000 * 60 * 60
