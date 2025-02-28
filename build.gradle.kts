4f960406482bc07716949d59a2d2d7c3795d58ef
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2025-02-28 16:55:54 +0200

    add dependencies for database

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,18 +24,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    // implementation("org.springframework.boot:spring-boot-starter-data-jpa") only when db is configured
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("io.jsonwebtoken:jjwt-gson:0.12.6")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    implementation("org.postgresql:postgresql:42.7.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.testcontainers:testcontainers:1.20.5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
