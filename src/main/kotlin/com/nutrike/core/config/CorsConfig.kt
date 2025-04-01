899afda90022704987a0523815f3d6d6987575d7
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2025-04-01 10:49:46 +0300

    add a cors configuration allowing requests from local frontend

---
+++ src/main/kotlin/com/nutrike/core/config/CorsConfig.kt
@@ -0,0 +1,31 @@
+package com.nutrike.core.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("http://localhost:4200")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH")
+        config.allowedHeaders =
+            listOf(
+                "Authorization",
+                "Accept",
+                "X-Requested-With",
+                "Content-Type",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers",
+            )
+        config.allowCredentials = true
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
+}
